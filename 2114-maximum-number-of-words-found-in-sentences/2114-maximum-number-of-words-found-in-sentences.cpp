1ac0709885621f07d3cb2488f170e24e303d3674
Shruti Singh <91374600+SHRUTI-SINGH16@users.noreply.github.com>
2022-10-28 09:02:06 +0530

    Time: 41 ms (8.62%), Space: 9.6 MB (99.67%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int largest = 0;
-        int count = 0;
+        int ans = INT_MIN;
         for(int i=0;i<sentences.size();++i){
+            int count = 0;
             for(int j=0;j<sentences[i].size();++j){
                 if(sentences[i][j]==' '){
                     count++;
                 }
             }
-            largest = max(1+count,largest);
-            count = 0;
+            ans = max(ans,count);
         }
-        return largest;
+        return ans+1;
     }
 };
